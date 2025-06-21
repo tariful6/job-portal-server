eb755d4dc942878954f85868599b2946fe42f8ad
tariful6 <tariful2306@gmail.com>
2025-06-21 15:24:31 +0600

    implement json web token

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin : ['http://localhost:5173'],
+  credentials : true
+}));
 app.use(express.json());
+app.use(cookieParser());
+
+
+const logger = (req, res, next) => {
+  console.log("logger enable");
+  next();
+}
+
+const verifyToken = (req, res, next) => {
+  // console.log("verify token enable", req?.cookies?.token);
+  const token =  req?.cookies?.token;  
+  // console.log(token);
+  if(!token){
+    return res.status(401).send({message : 'Unauthorized access'})
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded)=>{
+    if(err){
+      return res.send(401).send({message : 'Authorized accessed'})
+    }
+    req.user = decoded;
+    next();
+  })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yy3zscc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -26,8 +55,17 @@ async function run() {
 
     const jobCollection = client.db("jobPortal").collection("jobs");
     const applicationCollection = client.db("jobPortal").collection("applications");
+    
+    // auth related api -------------
+    app.post('/jwt', async(req, res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET, {expiresIn: '5h'})
+      res.cookie('token', token,{httpOnly : true, secure : false,}).send({success : true})
+    })
 
-    app.get("/jobs", async (req, res) => {
+    app.get("/jobs", logger, async (req, res) => {
+      console.log('inside the logger');
+      
       const email = req.query.email;
       let query = {};
       if(email){
@@ -50,10 +88,17 @@ async function run() {
       res.send(result)
     })
 
-    app.get("/jobApplication", async (req, res) => {
+    app.get("/jobApplication", verifyToken, async (req, res) => {
       const email = req.query.email;
       const query = { applicant_email: email };
+
+      if(req.user.email !== req.query.email){
+        return res.status(403).send({message : 'forbidden access'});
+      }
+      // console.log('cuk cuk cookie - ', req.cookies);
+
       const result = await applicationCollection.find(query).toArray();
+
 
       //   fokira way to aggregate data ---
       for (const application of result) {
