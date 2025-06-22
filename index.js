1aa3e5016ab120e098c67577875891c0e69c047f
tariful6 <tariful2306@gmail.com>
2025-06-22 09:53:50 +0600

    axios interceptor implement

--- index.js
+++ index.js
@@ -63,6 +63,16 @@ async function run() {
       res.cookie('token', token,{httpOnly : true, secure : false,}).send({success : true})
     })
 
+
+    app.post('/logout', (req, res)=>{
+      res.clearCookie('token', {
+        httpOnly : true,
+        secure : false
+      }).send({success : true})
+    })
+
+    // job related api -------------
+
     app.get("/jobs", logger, async (req, res) => {
       console.log('inside the logger');
       
